f4730bc1127a0e2382c5728564f0bef6c1d3abd9
Mst Rinky Akter <mstrinkya@gmail.com>
2022-10-22 23:12:31 +0600

    increasing decreasing value set

--- images/app.js
+++ images/app.js
@@ -1,8 +1,38 @@
-// console.log('connected');
-document.getElementById('case-plus').addEventListener('click',function(){
-    // console.log('clicked');
+// // console.log('connected');
+// document.getElementById('case-plus').addEventListener('click',function(){
+//     // console.log('clicked');
+//     const caseInput = document.getElementById('case-number');
+//     const caseNumber = caseInput.value ;
+//     caseInput.value = parseInt(caseNumber) + 1;
+//     // console.log(caseNumber);
+// })
+
+// document.getElementById('case-minus').addEventListener('click',function(){
+//     // console.log('clicked');
+//     const caseInput = document.getElementById('case-number');
+//     const caseNumber = caseInput.value;
+//     caseInput.value = parseInt(caseNumber)-1;
+//     //  console.log(caseNumber);
+// })
+
+
+function updateCaseNumber(isIncreasing){
     const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value ;
-    caseInput.value = parseInt(caseNumber) + 1;
-    console.log(caseNumber);
+    let caseNumber = caseInput.value ;
+    if (isIncreasing == true){
+       caseNumber = parseInt(caseNumber)+1;
+    }
+    else if (caseNumber>0){
+      caseNumber= parseInt(caseNumber)-1;
+    }
+    caseInput.value = caseNumber;
+    // update case total 
+     const caseTotal = document.getElementById('case-total');
+     caseTotal.innerText = caseNumber *59 ;    
+}
+document.getElementById('case-plus').addEventListener('click',function(){
+    updateCaseNumber(true);
+})
+document.getElementById('case-minus').addEventListener('click',function(){
+    updateCaseNumber(false);
 })
